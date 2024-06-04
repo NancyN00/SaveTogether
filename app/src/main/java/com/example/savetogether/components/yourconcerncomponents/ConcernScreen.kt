9c1447c141346bc071bb228feb9a15aea03abd6a
Nancy Nyabuti <105346686+NancyN00@users.noreply.github.com>
2024-06-04 06:27:59 +0300

    add feature to concern screen

--- app/src/main/java/com/example/savetogether/components/yourconcerncomponents/ConcernScreen.kt
+++ app/src/main/java/com/example/savetogether/components/yourconcerncomponents/ConcernScreen.kt
@@ -2,15 +2,26 @@ package com.example.savetogether.components.yourconcerncomponents
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ConcernScreen(){
+    
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { /*TODO*/ })
+        }
+    ) {
+        
+    }
     Column(
         modifier = Modifier.fillMaxSize()
         //remember to add the vertical scroll state.
     ){
+        
 
     }
 }
